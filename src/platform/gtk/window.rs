6f42b27059e9683d233c42ea70ce62feef95c1f4
Antoni Boucher <bouanto@zoho.com>
2018-03-26 14:12:26 -0400

    Commit

--- src/platform/gtk/window.rs
+++ src/platform/gtk/window.rs
@@ -3,11 +3,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use std::cell::RefCell;
+use std::mem;
 use std::rc::Rc;
 
+use gdk::{POINTER_MOTION_MASK, SCROLL_MASK};
 use gtk;
 use gtk::{
     ContainerExt,
+    GLArea,
+    GLAreaExt,
     Label,
     NotebookExt,
     NotebookExtManual,
@@ -47,43 +51,56 @@ impl WindowMethods for Window {
             format!("{} {} {:15.15} {}|", f, selected, title, loading)
         });*/
 
-        let mut windows = self.windows.borrow_mut();
+        let tabs;
+        let visual_count;
 
-        let state_count = state.browsers.len();
         {
-            let window = &windows[WINDOW_ID];
+            let mut windows = self.windows.borrow_mut();
 
-            if state_count > 1 {
-                window.tabs.set_show_tabs(true);
-            }
-            else {
-                //window.tabs.set_show_tabs(false); // TODO: uncomment.
-            }
-            for i in 0..state_count {
-                if let Some(ref title) = state.browsers[i].title {
-                    if Some(i) == state.current_browser_index {
-                        window.gtk_window.set_title(title);
+            let state_count = state.browsers.len();
+            {
+                let window = &windows[WINDOW_ID];
+
+                if state_count > 1 {
+                    window.tabs.set_show_tabs(true);
+                }
+                else {
+                    //window.tabs.set_show_tabs(false); // TODO: uncomment.
+                }
+                for i in 0..state_count {
+                    if let Some(ref title) = state.browsers[i].title {
+                        if Some(i) == state.current_browser_index {
+                            window.gtk_window.set_title(title);
+                        }
+                        if let Some(tab) = window.tabs.get_nth_page(Some(i as u32)) {
+                            window.tabs.set_tab_label_text(&tab, title);
+                        }
                     }
-                    if let Some(tab) = window.tabs.get_nth_page(Some(i as u32)) {
-                        window.tabs.set_tab_label_text(&tab, title);
-                    }
+                }
+
+                tabs = window.tabs.clone();
+                visual_count = window.tabs.get_n_pages() as usize;
+                if state_count == visual_count + 1 {
+                    let gl_area = GLArea::new();
+                    gl_area.set_auto_render(false);
+                    gl_area.set_has_depth_buffer(true);
+                    gl_area.add_events((POINTER_MOTION_MASK | SCROLL_MASK).bits() as i32);
+                    gl_area.set_vexpand(true);
+                    window.tabs.add(&gl_area);
+                    gl_area.show();
+                    gl_area.make_current();
+                    mem::forget(gl_area);
                 }
             }
 
-            let visual_count = window.tabs.get_n_pages() as usize;
-            if state_count == visual_count + 1 {
-                // FIXME: need a new GLArea or to always show the same one?
-                let widget = gtk::EventBox::new(); // FIXME: use another widget.
-                window.tabs.add(&widget);
-                widget.show();
+            if state.urlbar_focused {
+                let url = format!("{}", state.browsers[state.current_browser_index.unwrap()]
+                                  .url.as_ref().map_or("", |t| t.as_str()));
+                windows[WINDOW_ID].window_events.push(WindowEvent::UrlbarFocusChanged(false));
             }
         }
 
-        if state.urlbar_focused {
-            let url = format!("{}", state.browsers[state.current_browser_index.unwrap()]
-                              .url.as_ref().map_or("", |t| t.as_str()));
-            windows[WINDOW_ID].window_events.push(WindowEvent::UrlbarFocusChanged(false));
-        }
+        tabs.set_current_page(Some(visual_count as u32));
     }
 
     fn new_view(&self) -> Result<Rc<ViewMethods>, &'static str> {
